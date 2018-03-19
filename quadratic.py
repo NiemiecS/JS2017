3dfde2e299304fed060ab7710f848383b848efd5
NiemiecS <szymon19965@gmail.com>
2018-03-20 00:10:17 +0100

    Add files via upload

---
+++ quadratic.py
@@ -0,0 +1,18 @@
+﻿import sys
+from math import sqrt
+a=int(sys.argv[1])
+b=int(sys.argv[2])
+c=int(sys.argv[3])
+delta=(b**2)-(4*a*c)
+
+if(delta>0):
+	print(2)
+	x1 = ( -b - sqrt(delta)) / (2*a)
+	x2 = ( -b + sqrt(delta)) / (2*a)
+	print(x1,x2)
+if(delta==0):
+	print(1)
+	x=-b/(2*a)
+	print(x)
+if(delta<0):
+	print("0")
